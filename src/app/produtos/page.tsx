3cdbef088515a3a22e92d42eea48195956a9ee3e
STATION018\Ti Desenvolvedor <analista19.ti@connectsa.com.br>
2023-06-22 10:08:21 -0300

    first commit

---
+++ src/app/produtos/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+import React, { Component, useState } from 'react'
+
+export default function Teste() {
+  let [menu,setMenu] = useState(1);
+
+    const menu1 = () => {
+      return(
+        <div>menu 1</div>
+      )
+    }
+    return (
+      <>
+      <div className='box-1'></div>
+  
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-2xl">
+          
+          <div className=' bg-gray-200'>
+          <p className='text-gray-800'>Teste page 1</p>
+          <button className='mr-3  items-center justify-center rounded-md border border-transparent bg-lime-600 px-8 py-3 text-base font-medium text-white hover:bg-lime-800 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:ring-offset-2' onClick={() => setMenu(1)}>menu1</button>
+          <button className='mr-3  items-center justify-center rounded-md border border-transparent bg-lime-600 px-8 py-3 text-base font-medium text-white hover:bg-lime-800 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:ring-offset-2' onClick={() => setMenu(2)}>menu2</button>
+          <button className='mr-3  items-center justify-center rounded-md border border-transparent bg-lime-600 px-8 py-3 text-base font-medium text-white hover:bg-lime-800 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:ring-offset-2' onClick={() => setMenu(3)}>menu3</button>
+          <hr className='text-gray-800'></hr>
+          <p className='text-gray-800'>
+            {menu === 1 && menu1()}
+            {menu === 2 && "menu 2"}
+            {menu === 3 && "menu 3"}
+          </p>  
+         
+        </div>
+        </div>
+
+ 
+    </div>
+      </>
+      
+    )
+}
