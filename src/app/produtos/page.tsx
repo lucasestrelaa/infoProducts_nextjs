a2ef24fd89162af9387f52be56e7061a832c4115
STATION018\Ti Desenvolvedor <analista19.ti@connectsa.com.br>
2023-06-22 10:26:37 -0300

    atualizações 1

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -6,7 +6,17 @@ export default function Teste() {
 
     const menu1 = () => {
       return(
-        <div>menu 1</div>
+        <div className='text-gray-800'>menu 1 - teste</div>
+      )
+    }
+    const menu2 = () => {
+      return(
+        <div className='text-gray-800'>menu 2</div>
+      )
+    }
+    const menu3= () => {
+      return(
+        <div className='text-gray-800'>menu 3</div>
       )
     }
     return (
@@ -22,12 +32,9 @@ export default function Teste() {
           <button className='mr-3  items-center justify-center rounded-md border border-transparent bg-lime-600 px-8 py-3 text-base font-medium text-white hover:bg-lime-800 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:ring-offset-2' onClick={() => setMenu(2)}>menu2</button>
           <button className='mr-3  items-center justify-center rounded-md border border-transparent bg-lime-600 px-8 py-3 text-base font-medium text-white hover:bg-lime-800 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:ring-offset-2' onClick={() => setMenu(3)}>menu3</button>
           <hr className='text-gray-800'></hr>
-          <p className='text-gray-800'>
             {menu === 1 && menu1()}
-            {menu === 2 && "menu 2"}
-            {menu === 3 && "menu 3"}
-          </p>  
-         
+            {menu === 2 && menu2()}
+            {menu === 3 && menu3()}
         </div>
         </div>
 
